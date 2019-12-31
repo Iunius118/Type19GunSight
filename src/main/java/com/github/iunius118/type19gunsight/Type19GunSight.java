c482ecfc42a67463de8860a02bbbcdb087e9ee16
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2019-12-31 14:23:56 +0900

    Add config class

--- src/main/java/com/github/iunius118/type19gunsight/Type19GunSight.java
+++ src/main/java/com/github/iunius118/type19gunsight/Type19GunSight.java
@@ -1,6 +1,7 @@
 package com.github.iunius118.type19gunsight;
 
 import com.github.iunius118.type19gunsight.client.ClientEventHandler;
+import com.github.iunius118.type19gunsight.config.GunSightConfig;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -18,6 +19,8 @@ public class Type19GunSight {
     public static final String MOD_NAME = "Type 19 Gun Sight";
     public static final String MOD_VERSION = "1.12.2-0.0.1.0";
     public static Logger LOGGER;
+
+    public static final GunSightConfig CONFIG = new GunSightConfig();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
--- src/main/java/com/github/iunius118/type19gunsight/config/GunSightConfig.java
+++ src/main/java/com/github/iunius118/type19gunsight/config/GunSightConfig.java
@@ -2,7 +2,20 @@ package com.github.iunius118.type19gunsight.config;
 
 import com.github.iunius118.type19gunsight.Type19GunSight;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.common.config.Config.LangKey;
 
 @Config(modid = Type19GunSight.MOD_ID)
 public class GunSightConfig {
+    @Comment("Enable Type 19 Gun Sight")
+    @LangKey(Type19GunSight.MOD_ID + ".config.enable")
+    public static boolean enable = true;
+
+    @Comment("Color of marker on HUD, in ARGB8888 format")
+    @LangKey(Type19GunSight.MOD_ID + ".config.marker_color")
+    public static int markerColor = 0xFFFFFFFF;
+
+    @Comment("Settings of sights for each item, in JSON {\"id\":\"item_id\", \"v\":initialVelocity[0.0 - 20.0], \"r\":resistanceFactor[0.0 - 2.0], \"g\":gravityFactor[-5.0 - 5.0]}")
+    @LangKey(Type19GunSight.MOD_ID + ".config.item_id")
+    public static String[] sightSettings = {"{\"id\":\"minecraft:bow\",\"v\":3.0,\"r\":0.99,\"g\":0.05}", "{\"id\":\"mod_id:example\",\"v\":3.0,\"r\":0.99,\"g\":0.03}"};
 }
